cf25738c9e9531310f6e3f725a1838c9205a9edc
dmjcb <dmjcb@outlook.com>
2024-07-23 19:36:25 +0800

    删除部分文件

--- C&C++/Code/Class/运算符的重载.cpp
+++ C&C++/Code/Class/运算符的重载.cpp
@@ -1,38 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Complex {
-    public:
-        Complex() {
-            real=0,imag=0;
-        }
-        Complex(double r,double i) {
-            real=r;
-            imag=i;
-        }
-        Complex operator+(Complex &c2);
-        void display();
-    private:
-        double real;
-        double imag;
+class Complex
+{
+public:
+    Complex()
+    {
+        real = 0, imag = 0;
+    }
+    Complex(double r, double i)
+    {
+        real = r;
+        imag = i;
+    }
+    Complex operator+(Complex &c2);
+    void display();
+
+private:
+    double real;
+    double imag;
 };
-Complex Complex::operator+(Complex &c2) {
+Complex Complex::operator+(Complex &c2)
+{
     Complex c;
-    c.real=real+c2.real;
-    c.imag=imag+c2.imag;
+    c.real = real + c2.real;
+    c.imag = imag + c2.imag;
     return c;
 }
-void Complex::display() {
-    cout<<"("<<real<<"."<<imag<<"i)"<<endl;
+void Complex::display()
+{
+    cout << "(" << real << "." << imag << "i)" << endl;
 }
 
-int main() {
-    Complex c1(3,4),c2(5,-10),c3;
-    c3=c1+c2;
-    cout<<"c1=";
+int main()
+{
+    Complex c1(3, 4), c2(5, -10), c3;
+    c3 = c1 + c2;
+    cout << "c1=";
     c1.display();
-    cout<<"c2=";
+    cout << "c2=";
     c2.display();
-    cout<<"c3=";
+    cout << "c3=";
     c3.display();
 }
-
--- C&C++/Code/Class/运算符重载算矩阵之和.cpp
+++ C&C++/Code/Class/运算符重载算矩阵之和.cpp
@@ -1,33 +1,44 @@
-#include<iostream>
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Author: dmjcb
+ * @Email:
+ * @Date: 2024-07-19 16:00:27
+ * @LastEditors: dmjcb
+ * @LastEditTime: 2024-07-23 19:33:12
+ */
+#include <iostream>
 using namespace std;
 class Matrix
 {
-    public:
-        Matrix(){};
-        friend Matrix operator + (Matrix &a,Matrix &b);
-        void display_Value();
-    private:
-        float matr[2][3];    
+public:
+    Matrix() {};
+    friend Matrix operator+(Matrix &a, Matrix &b);
+    void display_Value();
+
+private:
+    float matr[2][3];
 };
 
-Matrix operator + (Matrix &a,Matrix &b)
+Matrix operator+(Matrix &a, Matrix &b)
 {
-    return Matrix(a+b);
-} 
+    return Matrix(a + b);
+}
 void Matrix::display_Value()
 {
     Matrix c;
-    for(int i=0;i<2;i++){
-        for(int j=0;j<3;j++){
-            cout<<c.matr[i][j];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << c.matr[i][j];
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 int main()
 {
-    Matrix a,b,c;
-    c=a+b;
+    Matrix a, b, c;
+    c = a + b;
     c.display_Value();
-    
 }
