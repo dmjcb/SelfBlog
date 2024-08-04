54e0a28a3816ed67000945c2c4ded648885421a4
dmjcb <dmjcb@outlook.com>
2024-08-04 17:51:05 +0800

    精简

--- Algorithms/Code/数学/求最大公约数（GCD）和最小公倍数(LCM)/gcd.cpp
+++
@@ -1,4 +0,0 @@
-int gcd(int a, int b) {
-    return a == 0 ? b : gcd(b % a, a);
-}
-/*最小公倍数=两整数的乘积 ÷最大公约数*/
--- C&C++/Code/运算符/按位与.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int a = 3;
-    int b = 5;
-    printf("%d", a & b);
-}
