23b0e96b79fd1a882b8f25f5ae210093a32fd231
dmjcb <dmjcb@outlook.com>
2024-10-26 12:03:16 +0800

    fix

--- Tool/Code/DelUnusedImage.py
+++ Tool/Code/DelUnusedImage.py
@@ -2,35 +2,35 @@
 import os
 from codecs import open
 
-# 提取指定目录下所有md文件中图片超链接
-def get_md_picture_url(dir_path):
-    urls = []
-    n = 0
-    for fpathe, dirs, fs in os.walk(dir_path):
-        if ".git" not in fpathe:
-            for f in fs:
-                name = os.path.join(fpathe, f)
-                if ".md" in name:
-                    with open(name, "rb", "utf-8", errors="ignore") as text:
-                        for line in text:
-                            line = line.replace("\r\n", "")
-                            if "/assets/SelfImgur/" in line:
-                                line = line[:-1]
-                                urls.append(line.split('/')[-1])
-                                n+=1
-    return urls
-
 def del_unused_img():
-    n = 0
-    md_path = "c:\\Users\\dmjcb\\Documents\\Code\\SelfBlog"
-    image_path = "c:\\Users\\dmjcb\\Documents\\Code\\SelfBlog\\assets\\SelfImgur"
-    
-    used_urls = get_md_picture_url(md_path)
+    # 提取指定目录下所有md文件中图片超链接
+    def get_md_picture_url(dir_path):
+        urls = []
+        url_num = 0
+        for fpathe, dirs, fs in os.walk(dir_path):
+            if ".git" not in fpathe:
+                for f in fs:
+                    name = os.path.join(fpathe, f)
+                    if ".md" in name:
+                        with open(name, "rb", "utf-8", errors="ignore") as text:
+                            for line in text:
+                                line = line.replace("\r\n", "")
+                                if "/assets/SelfImgur/" in line:
+                                    line = line[:-1]
+                                    urls.append(line.split('/')[-1])
+                                    url_num += 1
+        return urls
 
-    for fpathe, dirs, fs in os.walk(image_path):
+    del_num = 0
+    dir_path = "c:\\Users\\dmjcb\\Documents\\Code\\SelfBlog"
+    image_dir_path = dir_path + "\\assets\\SelfImgur"
+    
+    used_urls = get_md_picture_url(dir_path)
+
+    for fpathe, dirs, fs in os.walk(image_dir_path):
         for f in fs:
             if f not in used_urls:
-                n+=1
+                del_num += 1
                 name = os.path.join(fpathe, f)
                 os.remove(name)
             
