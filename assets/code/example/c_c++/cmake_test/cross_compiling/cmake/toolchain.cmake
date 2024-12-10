1786976980645ef871704c634f4ca7bee2af294a
dmjcb <dmjcb@outlook.com>
2024-12-10 19:22:43 +0800

    小修改

---
+++ assets/code/example/c_c++/cmake_test/cross_compiling/cmake/toolchain.cmake
@@ -0,0 +1,6 @@
+# cmake/toolchain.cmake
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
